a13e44deda0e07143ededc80431f4048e7790930
Philippe Birbaum <tree@gmx.ch>
2020-02-11 15:07:48 +0100

    Initial RavenDB support (#38)

---
+++ src/RavenDB.Tests/RavenDBResourceTests.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Raven.Client.Documents.Session;
+using Xunit;
+
+namespace Squadron
+{
+    public class RavenDBResourceTests : IClassFixture<RavenDBResource>
+    {
+        private readonly RavenDBResource _ravenDBResource;
+
+        public RavenDBResourceTests(RavenDBResource ravenDBResource)
+        {
+            _ravenDBResource = ravenDBResource;
+        }
+
+        [Fact]
+        public void CreateDatabase_StoreUser_NoError()
+        {
+            //Act
+            Action action = () =>
+            {
+                using (IDocumentSession session =
+                    _ravenDBResource.CreateDatabase("test").OpenSession())
+                {
+                    session.Store(new User
+                    {
+                        Id = "1",
+                        Name = "John"
+                    });
+                    session.SaveChanges();
+                };
+            };
+
+            //Assert
+            action.Should().NotThrow();
+        }
+
+        internal class User
+        {
+            public string Id { get; set; }
+
+            public string Name { get; set; }
+        }
+    }
+}
---
+++ src/RavenDB/RavenDBDefaultOptions.cs
@@ -0,0 +1,20 @@
+namespace Squadron
+{
+    /// <summary>
+    /// Default RavenDB resource options
+    /// </summary>
+    public class RavenDBDefaultOptions : ContainerResourceOptions
+    {
+        /// <summary>
+        /// Configure resource options
+        /// </summary>
+        /// <param name="builder"></param>
+        public override void Configure(ContainerResourceBuilder builder)
+        {
+            builder
+                .Name("ravendb")
+                .Image("ravendb/ravendb:ubuntu-latest")
+                .InternalPort(8080);
+        }
+    }
+}

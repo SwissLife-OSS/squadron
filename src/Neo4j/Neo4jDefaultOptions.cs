478c7ccb5ced4f4646cd6cbb9e55f9ffb5e6d440
Arif Hanif <arif.hanif3@gmail.com>
2020-05-29 19:44:02 -0400

    start neo4j addition

---
+++ src/Neo4j/Neo4jDefaultOptions.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Squadron
+{
+    /// <summary>
+    /// Default Neo4j resource options
+    /// </summary>
+    public class Neo4jDefaultOptions : ContainerResourceOptions, IComposableResourceOption
+    {
+        public Type ResourceType => typeof(MongoResource);
+
+        /// <summary>
+        /// Configure resource options
+        /// </summary>
+        /// <param name="builder"></param>
+        public override void Configure(ContainerResourceBuilder builder)
+        {
+            builder
+                .Name("neo4j")
+                .Image("neo4j:latest")
+                .InternalPort(7474);
+        }
+    }
+}
+
---
+++ src/Neo4j/Neo4jResource.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Neo4j.Driver;
+
+namespace Squadron
+{
+    public class Neo4jResource
+    {
+
+    }
+}

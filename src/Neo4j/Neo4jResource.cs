e643e55ac71774c73acd90c0061d5c180c68377b
Arif Hanif <arif.hanif3@gmail.com>
2020-05-30 11:58:40 -0400

    update solution to add projects

--- src/Neo4j/Neo4jResource.cs
+++ src/Neo4j/Neo4jResource.cs
@@ -1,15 +1,26 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using Neo4j.Driver;
+using Xunit;
 
 namespace Squadron
 {
-    public class Neo4jResource
-    {
+    public class Neo4jResource : Neo4jResource<Neo4jDefaultOptions> { }
 
+    /// <summary>
+    /// Represents a neo4j resource that can be used by unit tests.
+    /// </summary>
+    public class Neo4jResource<TOptions>
+        : ContainerResource<TOptions>,
+          IAsyncLifetime
+        where TOptions : ContainerResourceOptions, new()
+    {
+        /// <summary>
+        /// ConnectionString
+        /// </summary>
+        public string ConnectionString { get; private set; }
+
+        public async override Task InitializeAsync()
+        {
+            await base.InitializeAsync();
+        }
     }
 }

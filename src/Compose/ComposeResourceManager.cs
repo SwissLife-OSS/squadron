40146e823b5c9b9e957a81035e159ef1257068e1
Lucien Zimmermann <lucien.zimmermann@swisslife.ch>
2020-08-20 08:21:01 +0200

    Revert "Re-add whitespaces"
    This reverts commit f35f6a49e15ce460ded79c4a0c1c961ed1e16f83.

--- src/Compose/ComposeResourceManager.cs
+++ src/Compose/ComposeResourceManager.cs
@@ -1,9 +1,9 @@
-using System; 
-using System.Collections.Generic; 
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Linq;
 
-namespace Squadron 
+namespace Squadron
 {
     public class ComposeResourceManager
     {
@@ -57,4 +57,4 @@ namespace Squadron
             await Resource.DisposeAsync();
         }
     }
-} 
+}

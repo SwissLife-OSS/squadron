a557f5790723ee0bd75688d45800c76ec26e3eb3
Gabriel Lucaci <gabriel.lucaci.dev@gmail.com>
2020-10-02 13:44:35 +0200

    Initialize MongoDB database from files (#62)

---
+++ src/Mongo.Tests/MongoInitOptionResourceTests.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using FluentAssertions;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Xunit;
+
+namespace Squadron
+{
+    public class MongoInitOptionResourceTests
+        : IClassFixture<MongoResource<FileInitOptions>>
+    {
+        private readonly MongoResource<FileInitOptions> _mongoResource;
+
+        public MongoInitOptionResourceTests(MongoResource<FileInitOptions> mongoResource)
+        {
+            _mongoResource = mongoResource;
+        }
+
+        [Fact]
+        public async Task Database_IsInitialized()
+        {
+            IMongoCollection<BsonDocument> collection = _mongoResource.Client
+                .GetDatabase("fileImport")
+                .GetCollection<BsonDocument>("news");
+
+            List<BsonDocument> items = await collection
+                .Find(Builders<BsonDocument>.Filter.Empty)
+                .ToListAsync();
+
+            items.Should().HaveCount(1);
+        }
+    }
+
+    public class FileInitOptions : MongoInitOptions
+    {
+        public override CreateDatabaseFromFilesOptions GetOptions()
+        {
+            return new CreateDatabaseFromFilesOptions
+            {
+                DatabaseOptions = new CreateDatabaseOptions
+                {
+                    DatabaseName = "fileImport"
+                },
+                Files = new []
+                {
+                    new FileInfo(Path.Combine("Resources", "news.json"))
+                }
+            };
+        }
+    }   
+}

0d238ffd34a60d23a1229d687506925ce6384ba7
Arif Hanif <arif.hanif3@gmail.com>
2020-10-16 08:23:59 -0400

    Add Neo4j database (#53)
    Co-authored-by: Bejtulla Asani <mstraining_bejtulla@hotmail.com>
    Co-authored-by: Michael Staib <michael@chillicream.com>

---
+++ src/Neo4j/Neo4jDefaultOptions.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Squadron
+{
+    /// <summary>
+    /// Default Neo4j resource options
+    /// </summary>
+    public class Neo4jDefaultOptions 
+        : ContainerResourceOptions
+        , IComposableResourceOption
+    {
+        public Type ResourceType => typeof(Neo4jResource);
+
+        /// <summary>
+        /// Configure resource options
+        /// </summary>
+        /// <param name="builder"></param>
+        public override void Configure(ContainerResourceBuilder builder)
+        {
+            builder
+                .Name("neo4j")
+                .Image("neo4j:latest")
+                .InternalPort(7687)
+                .AddEnvironmentVariable("NEO4J_AUTH=none");
+        }
+    }
+}
---
+++ src/Neo4j/Neo4jResource.cs
@@ -0,0 +1,47 @@
+using System.Threading.Tasks;
+using Neo4j.Driver;
+using Xunit;
+
+namespace Squadron
+{
+    public class Neo4jResource : Neo4jResource<Neo4jDefaultOptions> { }
+
+    /// <summary>
+    /// Represents a neo4j database resource that can be used by unit tests.
+    /// </summary>
+    public class Neo4jResource<TOptions>
+        : ContainerResource<TOptions>
+        , IAsyncLifetime
+        where TOptions : ContainerResourceOptions, new()
+    {
+        /// <summary>
+        /// Neo4j database driver
+        /// </summary>
+        public IDriver Driver;
+
+        /// <summary>
+        /// ConnectionString
+        /// </summary>
+        public string ConnectionString { get; private set; }
+
+        public async override Task InitializeAsync()
+        {
+            await base.InitializeAsync().ConfigureAwait(false);
+
+            ConnectionString = $"bolt://{Manager.Instance.Address}:{Manager.Instance.HostPort}";
+
+            Driver = GraphDatabase.Driver(ConnectionString);
+            await Initializer.WaitAsync(new Neo4jStatus(Driver)).ConfigureAwait(false);
+        }
+
+        public IAsyncSession GetAsyncSession(string databaseName = null)
+        {
+            return Driver.AsyncSession(o => o.WithDatabase(databaseName ?? "neo4j"));
+        }
+
+        public void Dispose()
+        {
+            Driver?.Dispose();
+        }
+    }
+}

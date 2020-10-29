4a3930bd565bc4e37ee981dc7ff68a847232fc80
Bejtulla Asani <mstraining_bejtulla@hotmail.com>
2020-10-29 12:12:07 +0100

    Azure Cloud Storage Tests

--- src/AzureCloudStorage.Tests/AzureCloudStorageTests.cs
+++ src/AzureCloudStorage.Tests/AzureCloudStorageTests.cs
@@ -1,7 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
+using FluentAssertions;
 using Squadron;
 using Xunit;
 
@@ -29,9 +34,35 @@ namespace Squadron.AzureCloudStorage.Tests
         }
 
         [Fact(Skip = "Can not run without Azure credentials")]
-        public void Foo()
+        public async Task CreateBlobClient_UploadFile_ContentMatch()
         {
-            var a = "";
+            //Arrange
+            BlobContainerClient container = _storageAccount.CreateBlobContainerClient("foo");
+            await container.CreateIfNotExistsAsync();
+            var inputText = "Hello_AzureCloudStorage";
+            var data = Encoding.UTF8.GetBytes(inputText);
+            var inputStream = new MemoryStream(data);
+
+            //Act
+            BlobClient blobClient = container.GetBlobClient("test.txt");
+            await blobClient.UploadAsync(inputStream);
+
+            //Assert
+            BlobDownloadInfo downloaded = blobClient.Download();
+            var reader = new StreamReader(downloaded.Content);
+            var downloadedText = reader.ReadToEnd();
+            downloadedText.Should().Be(inputText);
+        }
+
+        [Fact(Skip = "Can not run without Azure credentials")]
+        public void ConnectionString_NotNull()
+        {
+            //Arrange & Act
+            var connectionString = _storageAccount.ConnectionString;
+
+            //Assert
+            connectionString.Should().NotBeNull();
+            connectionString.Should().Contain("BlobEndpoint");
         }
     }
 }

083f72111951ddcc63657a345cb89fd6ab1e16ef
Philippe <tree@gmx.ch>
2021-01-29 16:22:40 +0100

    Initial support for Azure KeyVault

---
+++ src/AzureCloudKeyVault.Core/KeyVaultOptionsBuilder.cs
@@ -0,0 +1,37 @@
+﻿using Squadron.AzureCloud;
+using Squadron.Model;
+
+namespace Squadron
+{
+    public class KeyVaultOptionsBuilder : AzureResourceOptionsBuilder
+    {
+        private KeyVaultModel _model = new KeyVaultModel();
+
+        /// <summary>
+        /// Creates a new empty builder
+        /// </summary>
+        /// <returns></returns>
+        public static KeyVaultOptionsBuilder New() => new KeyVaultOptionsBuilder();
+
+        private KeyVaultOptionsBuilder()
+            : base()
+        {
+        }
+
+        public KeyVaultOptionsBuilder Name(string name)
+        {
+            _model.Name = name;
+
+            return this;
+        }
+
+        /// <summary>
+        /// Builds the options
+        /// </summary>
+        /// <returns></returns>
+        public KeyVaultModel Build()
+        {
+            return _model;
+        }
+    }
+}
---
+++ src/AzureCloudKeyVault.Tests/TestAzureConfigResolver.cs
@@ -0,0 +1,21 @@
+using Microsoft.Extensions.Configuration;
+using Squadron.AzureCloud;
+
+namespace Squadron.AzureKeyVault.Tests
+{
+    public class TestAzureConfigResolver
+    {
+        internal static AzureResourceConfiguration Resolver()
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true)
+                .AddUserSecrets<AzureKeyVaultCreateNewResourceTests>()
+                .AddEnvironmentVariables()
+                .Build();
+
+            IConfigurationSection section = configuration.GetSection("Squadron:Azure");
+            AzureResourceConfiguration azureConfig = section.Get<AzureResourceConfiguration>();
+            return azureConfig;
+        }
+    }
+}

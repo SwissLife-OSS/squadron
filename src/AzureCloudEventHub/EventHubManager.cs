76839e63b31ea11c58d461721ee4a6ad49a1fa3e
Søren Søndergaard <s.sondergaard@icloud.com>
2021-06-03 14:43:47 +0200

    Create event hub namespace

--- src/AzureCloudEventHub/EventHubManager.cs
+++ src/AzureCloudEventHub/EventHubManager.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Threading.Tasks;
 using Microsoft.Azure.Management.EventHub;
+using Microsoft.Azure.Management.EventHub.Models;
+using Microsoft.Azure.Management.ServiceBus.Models;
 using Microsoft.Rest;
 using Squadron.AzureCloud;
+using SkuName = Microsoft.Azure.Management.EventHub.Models.SkuName;
 
 namespace Squadron
 {
@@ -29,6 +32,24 @@ namespace Squadron
                     SubscriptionId = _azureResourceIdentifier.SubscriptionId,
                 };
             }
+        }
+
+        internal async Task<string> CreateNamespaceAsync(string location)
+        {
+            await EnsureAuthenticatedAsync();
+
+            var ns = $"squadron-{Guid.NewGuid().ToString("N").Substring(8)}";
+            var pars = new EHNamespace
+            {
+                Sku = new Sku(SkuName.Standard),
+                Location = location
+            };
+
+            EHNamespace res = await _client.Namespaces
+                .CreateOrUpdateAsync(_azureResourceIdentifier.ResourceGroupName, ns, pars);
+
+            _azureResourceIdentifier.Name = res.Name;
+            return res.Name;
         }
     }
 }

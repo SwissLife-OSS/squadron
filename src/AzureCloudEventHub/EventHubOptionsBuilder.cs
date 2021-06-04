35cb1fd97ccd264c4c6ab5cfbf6059184649b95d
Daniel Bjerring Jørgensen <DBJ-aadadmin@energinet.dk>
2021-06-04 09:02:54 +0200

    add EventHub options builder

--- src/AzureCloudEventHub/EventHubOptionsBuilder.cs
+++ src/AzureCloudEventHub/EventHubOptionsBuilder.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Squadron.AzureCloud;
 using Squadron.Model;
 
@@ -8,12 +9,37 @@ namespace Squadron
     /// </summary>
     public class EventHubOptionsBuilder : AzureResourceOptionsBuilder
     {
+        private EventHubModel _model = new EventHubModel();
+
+        /// <summary>
+        /// Creates a new empty builder
+        /// </summary>
+        /// <returns></returns>
+        public static EventHubOptionsBuilder New() => new EventHubOptionsBuilder();
+
+
+        private EventHubOptionsBuilder()
+            : base()
+        {
+
+        }
+
+        /// <summary>
+        /// Namespace
+        /// </summary>
+        /// <param name="ns">The namespace.</param>
+        /// <returns></returns>
+        public EventHubOptionsBuilder Namespace(string ns)
+        {
+            _model.Namespace = ns;
+            return this;
+        }
         /// <summary>
         /// Build an <see cref="EventHubModel"/> from options
         /// </summary>
         public EventHubModel Build()
         {
-            return new EventHubModel();
+            return _model;
         }
     }
 }

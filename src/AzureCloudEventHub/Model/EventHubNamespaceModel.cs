aa775e65896e6cf2868714462b4b45f1513546fd
djorgensendk <32477395+djorgensendk@users.noreply.github.com>
2021-07-04 19:04:45 +0200

    Added support for Azure Event Hub (#98)

---
+++ src/AzureCloudEventHub/Model/EventHubNamespaceModel.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+
+namespace Squadron.Model
+{
+    /// <summary>
+    /// Azure Event Hub model
+    /// </summary>
+    public class EventHubNamespaceModel
+    {
+        private List<EventHubModel> eventHubs;
+
+        public EventHubNamespaceModel()
+        {
+            eventHubs = new List<EventHubModel>();
+        }
+
+        /// <summary>
+        /// Gets or sets the namespace.
+        /// </summary>
+        /// <value>
+        /// The namespace.
+        /// </value>
+        public string Namespace { get; set; }
+
+        /// <summary>
+        /// Gets or sets the provisioning mode.
+        /// </summary>
+        /// <value>
+        /// The provisioning mode.
+        /// </value>
+        internal EventHubProvisioningMode ProvisioningMode { get; set; }
+            = EventHubProvisioningMode.UseExisting;
+
+
+        public void AddEventHub(EventHubModel eventHub)
+        {
+            eventHubs.Add(eventHub);
+        }
+
+        public IEnumerable<EventHubModel> GetEventHubs()
+        {
+            return eventHubs;
+        }
+    }
+
+    /// <summary>
+    /// Defines Event Hub provisioning modes
+    /// </summary>
+    internal enum EventHubProvisioningMode
+    {
+        /// <summary>
+        /// The uan existing Azure resource
+        /// </summary>
+        UseExisting,
+
+        /// <summary>
+        /// Provision and delete resource
+        /// </summary>
+        CreateAndDelete
+    }
+}

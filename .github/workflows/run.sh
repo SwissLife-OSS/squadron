96c12bf2f09eef204787a6a0f09a000ea19a56aa
glucaci <gabriel.lucaci.dev@gmail.com>
2023-08-25 17:02:38 +0200

    Try

--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -2,8 +2,11 @@ run() {
   local cpu_core=$(taskset -c -p $$ 2>&1 | awk -F': ' '{print $2}')
   local total_cores=$(nproc)
 
-  echo -e "Running on CPU Core: $cpu_core of $total_cores"
-  echo -e "\033[0;34mRunning: $@\033[0m"
+  echo -e "┌─────────────────────────────────────────────┐"
+  echo -e "│ \033[0;34mRunning on CPU Core: $cpu_core of $total_cores\033[0m │"
+  echo -e "├─────────────────────────────────────────────┤"
+  echo -e "│ \033[0;34mRunning: $@\033[0m                               │"
+  echo -e "└─────────────────────────────────────────────┘"
 
   "$@"
 

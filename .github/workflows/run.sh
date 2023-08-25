b803e447e3bce239b4c501edf3661a565af3d614
glucaci <gabriel.lucaci.dev@gmail.com>
2023-08-25 16:55:28 +0200

    Cleanup

--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -1,4 +1,15 @@
 run() {
+  local start_time=$(date '+%Y-%m-%d %H:%M:%S')
+  local cpu_core=$(grep -m1 'processor' /proc/cpuinfo | awk '{print $3}')
+
+  echo "Start Time: $start_time"
+  echo "Running on CPU Core: $cpu_core"
   echo "Running: $@"
+
   "$@"
+
+  local end_time=$(date '+%Y-%m-%d %H:%M:%S')
+
+  echo "End Time: $end_time"
+  echo ""
 }

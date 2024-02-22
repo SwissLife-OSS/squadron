2c6412f035fdeaff9b93a87021b4abc95e82ec77
Normen Scheiber <nscheibe@live.com>
2024-02-22 16:20:54 +0100

    Fix build

--- src/Core/ContainerResourceSettings.cs
+++ src/Core/ContainerResourceSettings.cs
@@ -23,7 +23,7 @@ namespace Squadron
         /// Example: HostIp = 127.0.0.1 
         /// Default all IPs are allowed
         /// </summary>
-        public string? HostIp { get; internal set; };
+        public string? HostIp { get; internal set; }
 
         /// <summary>
         /// Returns the main internal port of the container
